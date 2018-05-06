57ea906e395ea238ae563ff22b527cb7d02b93a1
prabhu-raja <prabhuraja@gmail.com>
2018-05-05 21:15:18 -0400

    cart changes

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: { // =  data
-    products: []
+    products: [],
+    cart: []
   },
   getters: { // = computed
     availableProducts (state, getters) {
@@ -22,13 +23,38 @@ export default new Vuex.Store({
           resolve()
         })
       })
+    },
+    addProductToTheCart (context, product) {
+      if(product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id)
+        if(!cartItem) {
+          // Add to the cart
+          context.commit('pushProductToCart', product.id)
+        } else {
+          // Increase the cartItem Count
+          context.commit('increaseItemQuantity', cartItem)
+        }
+        context.commit('decrementProductInventory', product)
+      }
     }
 
   },
-  mutations: {
-    // It is responsible for setting and updating the state
+  mutations: { // It is responsible for setting and updating the state
+
     setProducts(state, prods) {
       state.products = prods
+    },
+    pushProductToCart (state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+    increaseItemQuantity (state, cartItem) {
+      cartItem.quantity++
+    },
+    decrementProductInventory (state, product) {
+      product.inventory--
     }
   }
 })
